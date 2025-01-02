e07b633d02896ff84a500b1af7aab451e823b962
DESKTOP-N6HP9TH\Van R <vannaranjo123@gmail.com>
2025-01-02 02:27:56 -0500

    Added compare page and put basic SASS

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
 function getJSONSync(url) {
-    var resp = "";
-    var xmlHttp = new XMLHttpRequest();
+  var resp = "";
+  var xmlHttp = new XMLHttpRequest();
 
-if (xmlHttp !== null) {
+  if (xmlHttp !== null) {
     xmlHttp.open("GET", url, false);
     xmlHttp.send();
     resp = xmlHttp.responseText;
+  }
+
+  return resp;
 }
 
-return resp;
-}
+function getDataSync() {
+  var tempURL =
+    "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
-  function getDataSync() {
-    var tempURL =
-      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  var jsonData = getJSONSync(tempURL);
+  var obj = JSON.parse(jsonData);
+  var pokemons = obj.pokemon;
 
-    var jsonData = getJSONSync(tempURL);
-    var obj = JSON.parse(jsonData);
-    var pokemons = obj.pokemon;
-
-    return (pokemons = JSON.parse(getJSONSync(tempURL)).pokemon);
+  return (pokemons = JSON.parse(getJSONSync(tempURL)).pokemon);
 }
 
 function retrievePokemonById(pokemonId) {
-var pokemons = getDataSync();
-for (var i = 0; i < pokemons.length; i++) {
+  var pokemons = getDataSync();
+  for (var i = 0; i < pokemons.length; i++) {
     if (pokemonId == pokemons[i].id) {
-    return pokemons[i];
+      return pokemons[i];
     }
-}
+  }
 }
 
 function outputData(pokemon) {
-var pNumber = document.getElementById("pNumber");
-var pName = document.getElementById("pName");
-var pHeight = document.getElementById("pHeight");
-var pWeight = document.getElementById("pWeight");
-var pImage = document.getElementById("pImage");
+  var pNumber = document.getElementById("pNumber");
+  var pName = document.getElementById("pName");
+  var pHeight = document.getElementById("pHeight");
+  var pWeight = document.getElementById("pWeight");
+  var pImage = document.getElementById("pImage");
 
-pNumber.innerHTML = pokemon.num;
-pName.innerHTML = pokemon.name;
-pHeight.innerHTML = pokemon.height;
-pWeight.innerHTML = pokemon.weight;
-pImage.src = pokemon.img;
-pImage.setAttribute("alt", pokemon.name);
+  pNumber.innerHTML = pokemon.num;
+  pName.innerHTML = pokemon.name;
+  pHeight.innerHTML = pokemon.height;
+  pWeight.innerHTML = pokemon.weight;
+  pImage.src = pokemon.img;
+  pImage.setAttribute("alt", pokemon.name);
 }
 
+// Add event listeners
+document.getElementById("Previous").addEventListener("click", () => {
+  const currentId = parseInt(document.getElementById("pNumber").textContent);
+  outputData(retrievePokemonById(currentId - 1));
+});
+
+document.getElementById("retrieve").addEventListener("click", () => {
+  const pokemonId = document.getElementById("pokemonId").value;
+  outputData(retrievePokemonById(pokemonId));
+});
+
+document.getElementById("Next").addEventListener("click", () => {
+  const currentId = parseInt(document.getElementById("pNumber").textContent);
+  outputData(retrievePokemonById(currentId + 1));
+});
